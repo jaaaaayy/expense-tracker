f1ac4d49a2a27aaa5006beddd9ae00de52b64ac6
Jay <jayvallespin51@gmail.com>
2025-03-24 22:15:23 +0800

    backup

--- routes/category.php
+++ routes/category.php
@@ -3,11 +3,11 @@
 use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth')->group(function () {
-  Route::get('categories', [CategoryController::class, 'index'])->name(name: 'categories.index');
-  Route::get('categories/create', [CategoryController::class, 'create'])->name(name: 'categories.create');
-  Route::post('categories', [CategoryController::class, 'store'])->name(name: 'categories.store');
-  Route::get('categories/{category}/edit', [CategoryController::class, 'edit'])->name(name: 'categories.edit');
-  Route::put('categories/{category}', [CategoryController::class, 'update'])->name(name: 'categories.update');
-  Route::delete('categories/{category}', [CategoryController::class, 'destroy'])->name(name: 'categories.destroy');
+Route::middleware('auth')->controller(CategoryController::class)->group(function () {
+  Route::get('categories', 'index')->name(name: 'categories.index');
+  Route::get('categories/create', 'create')->name(name: 'categories.create');
+  Route::post('categories', 'store')->name(name: 'categories.store');
+  Route::get('categories/{category}/edit', 'edit')->name(name: 'categories.edit');
+  Route::put('categories/{category}', 'update')->name(name: 'categories.update');
+  Route::delete('categories/{category}', 'destroy')->name(name: 'categories.destroy');
 });
--- routes/expense.php
+++ routes/expense.php
@@ -3,11 +3,11 @@
 use App\Http\Controllers\ExpenseController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth')->group(function () {
-  Route::get('expenses', [ExpenseController::class, 'index'])->name('expenses.index');
-  Route::get('expenses/create', [ExpenseController::class, 'create'])->name('expenses.create');
-  Route::post('expenses', [ExpenseController::class, 'store'])->name('expenses.store');
-  Route::get('expenses/{expense}/edit', [ExpenseController::class, 'edit'])->name('expenses.edit');
-  Route::put('expenses/{expense}', [ExpenseController::class, 'update'])->name('expenses.update');
-  Route::delete('expenses/{expense}', [ExpenseController::class, 'destroy'])->name('expenses.destroy');
+Route::middleware('auth')->controller(ExpenseController::class)->group(function () {
+  Route::get('expenses', 'index')->name('expenses.index');
+  Route::get('expenses/create', 'create')->name('expenses.create');
+  Route::post('expenses', 'store')->name('expenses.store');
+  Route::get('expenses/{expense}/edit', 'edit')->name('expenses.edit');
+  Route::put('expenses/{expense}', 'update')->name('expenses.update');
+  Route::delete('expenses/{expense}', 'destroy')->name('expenses.destroy');
 });
